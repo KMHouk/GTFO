0fabe54bffd33dffb64a1badc1e8987062c4d860
Kevin Houk <kmhouk@gmail.com>
2015-07-11 15:47:23 -0400

    Successful flight responses (needs to be parsed)

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,96 +1,28 @@
 require 'sabre_dev_studio'
 require 'sabre_dev_studio-flight'
 
-
 class PagesController < ApplicationController
+
+    before_action :get_flight_data, :only => [:flights]
+    before_action :require_user, :except => [:index]
 
     def home
     end
-
-    before_action :require_user, :except => [:index]
 
     def locations
     end
 
     def flights
-        @city = params[:city]
-        @flights = {
-          OriginLocation: "NYC",
-          Destinations: [{
-              Rank: 1,
-              Destination: {
-                  DestinationLocation: "CHI",
-                  CountryCode: "US",
-                  CountryName: "United States",
-                  RegionName: "North America",
-                  MetropolitanAreaName: "Chicago",
-                  Links: [{
-                      rel: "airportsInCity",
-                      href: "https://api.sabre.com/v1/lists/supported/cities/CHI/airports"
-                  }],
-                  Type: "City"
-              }
-          }, {
-              Rank: 2,
-              Destination: {
-                  DestinationLocation: "CUN",
-                  AirportName: "Cancun",
-                  CityName: "Cancun",
-                  CountryCode: "MX",
-                  CountryName: "Mexico",
-                  RegionName: "Latin America",
-                  Type: "Airport"
-              }
-          }, {
-              Rank: 3,
-              Destination: {
-                  DestinationLocation: "LAS",
-                  AirportName: "McCarran International Airport",
-                  CityName: "Las Vegas",
-                  CountryCode: "US",
-                  CountryName: "United States",
-                  RegionName: "North America",
-                  Type: "Airport"
-              }
-          }, {
-              Rank: 4,
-              Destination: {
-                  DestinationLocation: "ORL",
-                  CountryCode: "US",
-                  CountryName: "United States",
-                  RegionName: "North America",
-                  MetropolitanAreaName: "Orlando",
-                  Links: [{
-                      rel: "airportsInCity",
-                      href: "https://api.sabre.com/v1/lists/supported/cities/ORL/airports"
-                  }],
-                  Type: "City"
-              }
-          }, {
-              Rank: 5,
-              Destination: {
-                  DestinationLocation: "MIA",
-                  AirportName: "Miami International Airport",
-                  CityName: "Miami",
-                  CountryCode: "US",
-                  CountryName: "United States",
-                  RegionName: "North America",
-                  Type: "Airport"
-              }
-          }],
-          LookBackWeeks: 8,
-          Links: [{
-              rel: "self",
-              href: "https://api.sabre.com/v1/lists/top/destinations?origin=NYC&lookbackweeks=8&topdestinations=5"
-          }, {
-              rel: "linkTemplate",
-              href: "https://api.sabre.com/v1/lists/top/destinations?origin=<origin>&origincountry=<origincountry>&topdestinations=<topdestinations>&destinationtype=<destinationtype>&theme=<theme>&destinationcountry=<destinationcountry>®ion=<region>&lookbackweeks=<lookbackweeks>"
-          }]
-      }
-
     end
 
     def results
+    end
+
+    protected
+
+    def get_flight_data
+      @city = params[:city]
+      @flights = SabreDevStudio::Base.get("/v1/lists/top/destinations?origin=#{@city}&topdestinations=5")
     end
 
 end
