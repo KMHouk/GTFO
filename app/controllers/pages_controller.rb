ec3a09868f87ba896821423ca42448ad02ef4110
Clarissa Vu <cvu@co-opbranding.com>
2015-07-11 17:02:24 +0000

    Added Locations HTML/CSS

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,6 +5,9 @@ class PagesController < ApplicationController
     
     before_action :require_user, :except => [:index]
     
+    def locations
+    end
+    
 #the views of flights/ pics/ etc. should be listed under the "befre_action" command above.
 #any view we allow on the home page will be visible to all users, whether or not they're signed in.
     
