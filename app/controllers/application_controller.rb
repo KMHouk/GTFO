e89ace9cd4d69344f00d4934c4624497d0c69277
Clarissa Vu <cvu@co-opbranding.com>
2015-07-11 15:36:04 +0000

    THIS IS A TEST

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,11 +6,11 @@ class ApplicationController < ActionController::Base
   helper_method :current_user 
 
 	def current_user 
-  @current_user ||= User.find(session[:user_id]) if session[:user_id] 
+    @current_user ||= User.find(session[:user_id]) if session[:user_id] 
 	end
   
   def require_user 
-  redirect_to '/login' unless current_user 
+    redirect_to '/login' unless current_user 
 	end
 	
 end
