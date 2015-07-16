951a39adc7a4175549da48b129601f5a6a4bbab2
Kevin Houk <kmhouk@gmail.com>
2015-07-15 23:17:05 -0400

    fixed getting lat/lon to instagram api

--- app/controllers/instagram_controller.rb
+++ app/controllers/instagram_controller.rb
@@ -4,13 +4,13 @@ class InstagramController < ApplicationController
   def convert
     @insta = params[:choice]
     location = Geocoder.search(@insta)
-    @latitude = location[0].latitude
-    @longitude = location[0].longitude
-    render :index
+    @@latitude = location[0].latitude
+    @@longitude = location[0].longitude
+    redirect_to action: "index"
   end
 
   def index
-    @instagram = Instagram.media_search("#{@latitude}", "#{@longitude}", {:count => 12})
+    @instagram = Instagram.media_search("#{@@latitude}", "#{@@longitude}", {:count => 12})
   end
 
 end
