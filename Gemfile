b791cb52c3be9b4cba4466300b3a07fd33e9a166
Kevin Houk <kmhouk@gmail.com>
2015-07-16 22:00:30 -0400

    arrival city var working, and small text changes

--- Gemfile
+++ Gemfile
@@ -41,6 +41,8 @@ group :production do
   gem 'rails_12factor'
 end
 
+gem 'pry'
+
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/controllers/instagram_controller.rb
+++ app/controllers/instagram_controller.rb
@@ -1,16 +1,16 @@
-require 'geocoder'
-
 class InstagramController < ApplicationController
+
   def convert
     @insta = params[:choice]
     location = Geocoder.search(@insta)
     @@latitude = location[0].latitude
     @@longitude = location[0].longitude
-    redirect_to action: "index"
+    redirect_to action: "index", :arrival_city => @insta
   end
 
   def index
     @instagram = Instagram.media_search("#{@@latitude}", "#{@@longitude}", {:count => 18})
+    @arrival_city = params[:arrival_city]
   end
 
 end
