54271ec1e68a6044d8386de0d4965e992e96682c
Kevin Houk <kmhouk@gmail.com>
2016-03-13 19:27:47 -0400

    misc changes

--- config/initializers/instagram.rb
+++ config/initializers/instagram.rb
@@ -3,3 +3,9 @@ Instagram.configure do |config|
     config.client_id = "f0351e65e8d8436b80060c45e617f6c9"
     config.access_token = "522222831.f0351e6.676192f834984f90b4d6311c5af2a653"
 end
+
+# require "instagram"
+# Instagram.configure do |config|
+#     config.client_id = ENV['INSTRAGRAM_CLIENT_ID']"f0351e65e8d8436b80060c45e617f6c9"
+#     config.access_token = "522222831.f0351e6.676192f834984f90b4d6311c5af2a653"
+# end
